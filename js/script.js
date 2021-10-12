f830225452000884e68ff133ffad69bc926e27f6
AnVT <anvt96@gmail.com>
2021-10-13 05:05:02 +0700

    back to top button JS

--- js/script.js
+++ js/script.js
@@ -12,3 +12,61 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active")
     })
 });
+var v = 1;
+
+toggleBackToTop();
+$(window).scroll(function(event) {
+	 //Act on the event 
+	// console.log($(window).scrollTop());
+
+	// console.log($("#portfolio").offset().top);
+	if($(window).scrollTop() >= $("#banner").offset().top){
+		// $("nav").addClass('fixed-top');
+		// $("nav").addClass('dummy-padding');
+	}else {
+		// $("nav").removeClass('fixed-top');
+		// $("nav").removeClass('dummy-padding');
+	}
+	toggleBackToTop();
+});
+
+
+
+$(".back-to-top").click(function(event) {
+	/* Act on the event */
+	//time = distance / v;
+	var distance = $(this).offset().top;
+	var time = distance / v;
+	$("html, body").animate({scrollTop:0}, time);
+});
+
+function toggleBackToTop(){
+	if($(window).scrollTop() == 0){
+		//tắt nút back to top
+		$(".back-to-top").fadeOut();
+	}else {
+		//hiện ra
+		$(".back-to-top").fadeIn();
+	}
+}
+
+$("nav ul li a, #banner a").click(function(event) {
+	/* Act on the event */
+	event.preventDefault(); //ngăn chặn không cho chạy đến vùng có id tương ứng vs hash.
+	// var hash = $(this).attr("href");
+	var hash = this.hash; //hàm của javascript.
+	// alert(hash);
+	var target = $(hash);
+	//phần tử có id tương ứng vs hash của nút dc click.
+	var top = $(target).offset().top;
+	// alert(top);
+	var distance = Math.abs($(this).offset().top - top);
+	//distance: vùng hiện tại con trỏ chuột -  section id
+	var time = distance / v;
+
+	$("html, body").stop().animate({scrollTop: top}, time, function(){
+		console.log(1);
+		window.location.hash=hash;//upate hash of address
+	});
+	console.log(5);
+});
